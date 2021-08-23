7114171cdc4abf7d11c26f263ab6c19d26f5a183
PedroAvila <minimainor_austin@hotmail.com>
2021-08-23 08:24:16 -0500

    Opciones del menu interactivo

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 require('colors');
-const { inquirerMenu } = require('./helpers/inquirer');
+const { inquirerMenu, pausa } = require('./helpers/inquirer');
 
 
 console.clear();
@@ -14,7 +14,7 @@ const main = async() => {
         opt = await inquirerMenu();
         console.log({ opt });
 
-        
+        await pausa();
 
     } while( opt !== '0' );
 
